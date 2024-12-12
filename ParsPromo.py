ead83e6d3932b1f067648a22ee8bb3018fb5920d
ERETLY <68462812+ERETLY@users.noreply.github.com>
2024-12-12 17:29:26 +0300

    Update ParsPromo.py

--- ParsPromo.py
+++ ParsPromo.py
@@ -64,6 +64,7 @@ def extract_promo_code(message):
         r'Промоко[:\s\*`]*([A-Z0-9-]+)',   # Misspelled "Промокод"
         r'Промо[:\s\*`]*([A-Z0-9-]+)',     # Shortened "Промо"
         r'([A-Z0-9]{4}-[A-Z0-9]{4})',      # Pattern like Y088-GS22
+        r'([A-Z0-9]{5}-[A-Z0-9]{5})',      # Pattern like 2Y088-GGS22
         r'([A-Z0-9-]{5,})'                 # Any sequence of 5 or more uppercase letters, numbers, or hyphens
     ]
     
